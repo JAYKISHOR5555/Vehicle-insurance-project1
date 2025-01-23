fe52362efaa9256b741aac80c85630c90575b3a5
Jay <jaykishor5555@gmail.com>
2025-01-23 16:19:54 +0530

    base made

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jay kishor",
+    author_email="jaykishor5555@gmail.com",
+    packages=find_packages()
+)
